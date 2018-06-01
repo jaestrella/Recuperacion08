a6f591dfc9b3a189ad41e8c1e289aba08e5ad78d
jaestrella <jaestrella1991@gmail.com>
2018-06-01 06:59:58 +0200

    terminada

--- app/src/main/java/com/iesvirgendelcarmen/dam/recuperacion08/Activity2.java
+++ app/src/main/java/com/iesvirgendelcarmen/dam/recuperacion08/Activity2.java
@@ -2,6 +2,7 @@ package com.iesvirgendelcarmen.dam.recuperacion08;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.EditText;
@@ -14,29 +15,21 @@ import java.util.ArrayList;
  */
 
 public class Activity2 extends AppCompatActivity {
-    TextView tv2,tv3,tvValores;
-
+    private TextView mTv;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
 
-        tv2=(TextView)findViewById(R.id.tv2);
-        tv3=(TextView)findViewById(R.id.tv3);
-        tvValores=(TextView)findViewById(R.id.valores);
-        Intent intent=getIntent();
-        Bundle b=intent.getExtras();
+        mTv = (TextView) findViewById(R.id.texto2);
 
-        if(b!=null)
-        {
-            String j =(String) b.get("name");
-            tv2.setText(j);
-            String f =(String) b.get("fecha");
-            tv3.setText(f);
-            ArrayList<String> array = (ArrayList<String>) b.getStringArrayList("valores");
-            String v=(String) b.get("valores");
+        Bundle extras = getIntent().getExtras();
 
-        }
+        mTv.setText(extras.get("fecha").toString() + "\n");
+        mTv.append(extras.get("Primavera").toString() + "\n");
+        mTv.append(extras.get("Verano").toString() + "\n");
+        mTv.append(extras.get("Otoño").toString() + "\n");
+        mTv.append(extras.get("Invierno").toString() + "\n");
 
     }
 }
--- app/src/main/java/com/iesvirgendelcarmen/dam/recuperacion08/MainActivity.java
+++ app/src/main/java/com/iesvirgendelcarmen/dam/recuperacion08/MainActivity.java
@@ -1,7 +1,13 @@
 package com.iesvirgendelcarmen.dam.recuperacion08;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
+import android.app.SearchManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,53 +16,65 @@ import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    Button boton;
-    TextView tv1,tvDate,valor1,valor2,valor3,valor4,valor5,valoresTotales;
+    private Button mBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtn = (Button) findViewById(R.id.button1);
 
-        boton=(Button)findViewById(R.id.boton);
-        tv1=(TextView)findViewById(R.id.tv1);
-        tvDate = (TextView) findViewById(R.id.tvDate);
-        valor1=(TextView)findViewById(R.id.v1);
-        valor2=(TextView)findViewById(R.id.v2);
-        valor3=(TextView)findViewById(R.id.v3);
-        valor4=(TextView)findViewById(R.id.v4);
-        valor5=(TextView)findViewById(R.id.v5);
-        valoresTotales=(TextView)findViewById(R.id.valoresTotales);
-
-        boton.setOnClickListener(new View.OnClickListener() {
+        mBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String s=tv1.getText().toString();
-
-                SimpleDateFormat sdf = new SimpleDateFormat("EEE, MMM d, yyyy");
-                String date = sdf.format(new Date(System.currentTimeMillis()));
-                tvDate.setText(date);
-                String f=tvDate.getText().toString();
-
-                String v1=valor1.getText().toString();
-                String v2=valor2.getText().toString();
-                String v3=valor3.getText().toString();
-                String v4=valor4.getText().toString();
-                String v5=valor5.getText().toString();
-                ArrayList<String> array = new ArrayList<String>();
-                array.add(v1);
-                array.add(v2);
-                array.add(v3);
-                array.add(v4);
-                array.add(v5);
-
-                Intent intent=new Intent(getApplication(),Activity2.class);
-                intent.putExtra("name",s);
-                intent.putExtra("fecha",f);
-                intent.putStringArrayListExtra("myArr",array);
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Activity2.class);
+                intent.putExtra("fecha", Calendar.getInstance().get(Calendar.YEAR));
+                intent.putExtra("Primavera", "primavera");
+                intent.putExtra("Verano", "verano");
+                intent.putExtra("Otoño", "otoño");
+                intent.putExtra("Invierno", "invierno");
                 startActivity(intent);
+
+                /*Intent implicito = new Intent(Intent.ACTION_VIEW);
+                implicito.setData(Uri.parse("https://www.iesvirgendelcarmen.com/"));
+                startActivity(implicito);*/
+
+                /*Intent implicito2 = new Intent(Intent.ACTION_WEB_SEARCH);
+                String cadena = "JAÉN";
+                implicito2.putExtra(SearchManager.QUERY, cadena);
+                startActivity(implicito2);*/
+
+                /*Intent telefono = new Intent(Intent.ACTION_DIAL,
+                Uri.parse("tel:1234567890"));
+                startActivity(telefono);*/
+
+                /*Intent telefono2 = new Intent(Intent.ACTION_CALL);
+                telefono2.setData(Uri.parse("tel:1234567890"));
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(telefono2);*/
+
+                /*Intent mapa = new Intent(Intent.ACTION_VIEW);
+                mapa.setData(Uri.parse("geo:0,0?z=30&q=cines"));
+                startActivity(mapa);*/
+
+                /*Intent camara = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(camara);*/
+
+                /*Intent contactos = new Intent(Intent.ACTION_VIEW,Uri.parse("content://contacts/people/"));
+                startActivity(contactos);*/
             }
         });
     }
